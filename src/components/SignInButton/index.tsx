c5ea15f10da6d4bdedcf064a3e7d970a7844d250
João Azevedo <joao.azevedo@ufv.br>
2021-08-26 14:48:59 -0300

    adicionando next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,21 +1,35 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi'; 
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#84D361"/>
-      João Azevedo
-      <FiX color="#737380" className={styles.closeIcon}/>
-    </button>
-  ) : (
-    <button type="button" className={styles.signInButton}>
-      <FaGithub color="#EBA417"/>
-      Sign in with GitHub
-    </button>
+  return (
+    session ? (
+      <button 
+        type="button" 
+        className={styles.signInButton}
+      >
+        <FaGithub color="#84D361"/>
+        {session.user.name}
+        <FiX
+          color="#737380"
+          className={styles.closeIcon}
+          onClick={() => signOut()}
+        />
+      </button>
+    ) : (
+      <button 
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signIn('github')}
+      >
+        <FaGithub color="#EBA417"/>
+        Sign in with GitHub
+      </button>
+    )
   );
 }
